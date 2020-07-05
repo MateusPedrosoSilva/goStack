96509e1cf2b4b36fe85cd695abc9dfa57746bf42
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-07-05 10:50:40 -0300

    Starting the React.JS

--- semana02/primeiroProjetoNode/src/server.ts
+++ semana02/primeiroProjetoNode/src/server.ts
@@ -1,20 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 //dependencies
 app.use(express.json());
 
-app.post('/users', (req, res) => {
-    const { name, email } = req.body;
-
-    const user = {
-        name,
-        email,
-    };
-    
-    return res.json( user );
-});
+app.use(routes);
 
 app.listen(3333,()=>{
     console.log('🐺 Server is running on port 3333');
