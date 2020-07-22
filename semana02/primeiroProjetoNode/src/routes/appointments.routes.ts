96e7f41785206af83cebc3b75934aead47e04c88
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2020-07-21 23:33:19 -0300

    Configuring TypeORM into project.

--- semana02/primeiroProjetoNode/src/routes/appointments.routes.ts
+++ semana02/primeiroProjetoNode/src/routes/appointments.routes.ts
@@ -2,23 +2,25 @@ import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/appointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import { getCustomRepository } from 'typeorm';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (req, res)=>{
-    return res.status(200).json(appointmentRepository.all());
+appointmentsRouter.get('/', async (req, res)=>{
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
+    return res.status(200).json(appointments);
 })
 
-appointmentsRouter.post('/', (req, res) => {
+appointmentsRouter.post('/', async  (req, res) => {
     try {
         const { provider, date } = req.body;
-
+        
         const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService(appointmentRepository);
+        const createAppointment = new CreateAppointmentService();
 
-        const appointment = createAppointment.execute({provider, date: parsedDate});
+        const appointment = await createAppointment.execute({provider, date: parsedDate});
 
         return res.status(200).json(appointment);
     } catch(err){
--- semana02/primeiroProjetoNode/src/services/CreateAppointmentService.ts
+++ semana02/primeiroProjetoNode/src/services/CreateAppointmentService.ts
@@ -1,6 +1,7 @@
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/appointmentsRepository';
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 interface Request {
     provider: string;
@@ -8,26 +9,24 @@ interface Request {
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ provider, date }: Request): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-    constructor(appointmentsRepository: AppointmentsRepository){
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({ provider, date }: Request): Appointment {
         const appointmentDate = startOfHour(date);
     
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentInSameDate) {
             throw Error('This appointment is already booked!');
             // return res.status(400).json({ message: 'This appointment is already booked!'});
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate,
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
